496f7f2c464dd9ced2859ea3e01a21156c8085b6
ce-dric <song921216@gmail.com>
2023-12-11 14:16:35 +0900

    upload local files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace sagittarius
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void LoadDirectoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add("*");
+
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                // Set the text of the text boxes
+                DirectoryNameTextBox.Text = folder.DisplayName;
+                DirectoryPathTextBox.Text = folder.Path;
+
+                // Optionally process files in the directory
+                // var files = await folder.GetFilesAsync();
+                // foreach (StorageFile file in files)
+                // {
+                //     // Do something with each file
+                // }
+            }
+            else
+            {
+                // The user cancelled the picking operation
+                // Optionally clear the text boxes
+                DirectoryNameTextBox.Text = "";
+                DirectoryPathTextBox.Text = "";
+            }
+        }
+    }
+}
